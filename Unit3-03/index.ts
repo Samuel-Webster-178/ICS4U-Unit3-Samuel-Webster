073f7e604435acb770ba2659128420927cf532af
Samuel Webster <samuel.webster>
2024-05-01 09:39:04 -0400

    sdfgh

--- Unit3-03/index.ts
+++ Unit3-03/index.ts
@@ -8,25 +8,47 @@
 
 import { createPrompt } from 'bun-promptx'
 
+const MIN = 0
+const MAX = 999
+const ARRAY_SIZE = 50
+
 function binarySearch(userArray: number[], userNumber: number,
   lowIndex: number, highIndex: number) {
-  let rVal: number = -1;
+  let rVal: number = -1
   if (lowIndex <= highIndex) {
-    let midIndex: number = (lowIndex + highIndex) / 2;
+    let midIndex: number = Math.floor((lowIndex + highIndex) / 2)
     if (userArray[midIndex] == userNumber) {
-      rVal = midIndex;
+      rVal = midIndex
     } else if (userArray[midIndex] > userNumber) {
-      rVal = binarySearch(userArray, userNumber, lowIndex, midIndex - 1);
+      rVal = binarySearch(userArray, userNumber, lowIndex, midIndex - 1)
     } else {
-      rVal = binarySearch(userArray, userNumber, midIndex + 1, highIndex);
+      rVal = binarySearch(userArray, userNumber, midIndex + 1, highIndex)
     }
   }
-  return rVal;
+  return rVal
 }
 
-const userInput = createPrompt(`Entered = `)
-const inputInt = parseInt(userInput.value)
-let factorialOfInt = factorial(inputInt)
-console.log(`Returned = ${factorialOfInt}`)
+let numberArray: number[] = []
 
+for (let i = 0; i < ARRAY_SIZE; i++) {
+  numberArray[i] = Math.floor(Math.random() * (MAX + 1))
+}
+
+numberArray = numberArray.sort((a, b) => a - b)
+
+console.log("\nSorted list of numbers:\n")
+console.log(numberArray)
+
+try {
+  const userInput = createPrompt(
+    `What number are you searching for in the array? (integer between 0 and 999): `
+  )
+  let inputInt = parseInt(userInput.value)
+  if (inputInt > MAX || inputInt < MIN) {
+    throw Error()
+  }
+  console.log(`Returned = ${binarySearch(numberArray, inputInt, 0, numberArray.length - 1)}`)
+} catch {
+  console.log(`Invalid input.`)
+}
 console.log(`\nDone.`)
